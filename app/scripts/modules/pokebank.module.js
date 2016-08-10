c4e5350cd75d73b3dc88acea424123650cb5826a
gise88 <gianluca.iselli@gmail.com>
2016-08-10 23:28:18 +0200

    add info in renderPokemon

--- app/scripts/modules/pokebank.module.js
+++ app/scripts/modules/pokebank.module.js
@@ -20,27 +20,87 @@ $(function() {
 
         renderPokemon: function(pokemon, id, inverse) {
 
-          var popOver = '<p><strong>CP: </strong>'+ pokemon.cp +' <br/> <strong>IV: </strong>'+ pokemon.iv +' <br/> <strong>Stats: </strong>'+ pokemon.stats +'</p>';
-
+			//console.log(pokemon);
+			
+			var popOver = '';
+			if (pokemon.nickname !== undefined && pokemon.nickname.length > 0)
+				popOver += '<strong>Nickname: </strong> '+ pokemon.nickname + '<br/>';
+			
+			popOver += '<strong>CP: </strong>'+ pokemon.cp;
+			if (pokemon.maxCp !== undefined && pokemon.absMaxCp !== undefined)
+				popOver += ' / ' + pokemon.maxCp + ' (' + pokemon.absMaxCp + ')';
+			
+			if (pokemon.individualAttack !== undefined && pokemon.individualDefense !== undefined && pokemon.individualStamina !== undefined && pokemon.stamina !== undefined) {
+				popOver +=
+				'<table style="margin: 5px 0;">' +
+				' <thead>' +
+				'  <tr>' +
+				'   <th style="text-align: center; padding-right: 5px;">IV</th>' +
+				'   <th style="text-align: center; padding: 0 5px;">Atk</th>' +
+				'   <th style="text-align: center; padding: 0 5px;">Def</th>' +
+				'   <th style="text-align: center; padding: 0 5px;">Sta</th>';
+				
+				if (pokemon.level !== undefined)
+					popOver += '   <th style="text-align: center; padding: 0 5px;" >LVL</th>';
+				
+				popOver +=
+				'   <th style="text-align: center; padding: 0 5px;" >HP</th>' +
+				'  </tr>' +
+				' </thead>' +
+				' <tbody>' +
+				'  <tr>' +
+				'   <td style="text-align: center; padding-right: 5px;">' + pokemon.iv + '%</td>' +
+				'   <td style="text-align: center; padding: 0 5px;">' + pokemon.individualAttack + '</td>' +
+				'   <td style="text-align: center; padding: 0 5px;">' + pokemon.individualDefense + '</td>' +
+				'   <td style="text-align: center; padding: 0 5px;">' + pokemon.individualStamina + '</td>';
+				
+				if (pokemon.level !== undefined)
+					popOver += '   <td style="text-align: center; padding: 0 5px;">' + pokemon.level + '</td>';
+				
+				popOver += 
+				'   <td nowrap style="text-align: center; padding: 0 5px;">' + pokemon.stamina + (pokemon.maxStamina !== undefined ? ' / ' + pokemon.maxStamina : '') + '</td>' +
+				'  </tr>' +
+				' </tbody>' +
+				'</table>';
+			} else
+				popOver += '<br/> <strong>IV: </strong>'+ pokemon.iv + '<br/>';
+			
+			if (pokemon.move1 !== undefined && pokemon.move2 !== undefined) {
+				popOver += '<span style="white-space: nowrap;"><strong>Move 1: </strong>' + pokemon.move1 + '</span><br/>';
+				popOver += '<span style="white-space: nowrap;"><strong>Move 2: </strong>' + pokemon.move2 + '</span><br/>';
+			}
+			
+			if (pokemon.candy !== undefined && pokemon.candiesToEvolve !== undefined) {
+				popOver += '<strong>Candy: </strong>'+ pokemon.candy;
+				if (pokemon.candiesToEvolve > 0)
+					popOver += ' (' + pokemon.candiesToEvolve + '<strong> to evolve</strong>)';
+				else
+					popOver += ' (<strong>cannot evolve</strong>)';
+			}
+			
+			
+			
+			
           var elem = $('<span id="pokemon-id-' + id + '" class="col-sm-4 col-lg-3"><img class="media-object img-thumbnail" src="'+ icons.pokemon(pokemon.pokemonId) +'" alt="'+ pokemon.name +'" /></span>').popover({
             title: pokemon.name,
             html: true,
             placement: 'top',
             content: popOver,
-            trigger: 'hover'
+			trigger: 'hover'//'hover click'
           });
 
           if (inverse) {
-
             elements.panel.find('.pokemon-list').prepend(elem);
-            elements.panel.find('#pokemon-id-' + id ).append('<span class="badge">New</span>')
-
+            elements.panel.find('#pokemon-id-' + id).append('<span class="badge">New</span>');
           } else {
-
             elements.panel.find('.pokemon-list').append(elem);
-
+			
+			if (pokemon.favorite !== undefined && pokemon.favorite)
+				elements.panel.find('#pokemon-id-' + id).append('<span class="badge" style="color: yellow;"><i class="fa fa-star"></i></span>');
+			
+			if (pokemon.deployedFortId !== undefined && pokemon.deployedFortId.length > 0)
+				elements.panel.find('#pokemon-id-' + id).append('<span class="badge" style="left: 0px; right: auto;"><i class="fa fa-fort-awesome"></i></span>');
           }
-
         },
 
         init: function() {
